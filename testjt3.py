724e0727dd2e5d7c658ddb159d11d95a55641c27
Kai0220 <wenkai0638@gmail.com>
2023-11-20 15:43:13 +0800

    new file:   testjt3.py

---
+++ testjt3.py
@@ -0,0 +1,130 @@
+import itertools
+import os
+
+import matplotlib.pylab as plt
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_hub as hub
+
+print("TF version:", tf.__version__)
+print("Hub version:", hub.__version__)
+print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+model_name = "mobilenet_v3_large_075_224"
+model_handle_map = {
+    "mobilenet_v3_large_075_224": "https://tfhub.dev/google/imagenet/mobilenet_v3_large_075_224/feature_vector/5",
+}
+
+model_handle = model_handle_map.get(model_name)
+pixels = 224  # Adjust the image size for the specific model
+
+print(f"Selected model: {model_name} : {model_handle}")
+
+IMAGE_SIZE = (pixels, pixels)
+print(f"Input size {IMAGE_SIZE}")
+
+BATCH_SIZE = 16
+
+# Set the data directory to the 'train' directory in the same location as the script
+data_dir = os.path.join(os.path.dirname(__file__), 'train')
+
+# Check if the directory exists
+if not os.path.exists(data_dir):
+    print(f"The specified local path '{data_dir}' does not exist.")
+else:
+    print(f"Local data directory: {data_dir}")
+
+def build_dataset(subset):
+  return tf.keras.preprocessing.image_dataset_from_directory(
+      data_dir,
+      validation_split=.20,
+      subset=subset,
+      label_mode="categorical",
+      seed=123,
+      image_size=IMAGE_SIZE,
+      batch_size=1)
+
+train_ds = build_dataset("training")
+class_names = tuple(train_ds.class_names)
+train_size = train_ds.cardinality().numpy()
+train_ds = train_ds.unbatch().batch(BATCH_SIZE)
+train_ds = train_ds.repeat()
+
+normalization_layer = tf.keras.layers.Rescaling(1. / 255)
+preprocessing_model = tf.keras.Sequential([normalization_layer])
+do_data_augmentation = False
+if do_data_augmentation:
+  preprocessing_model.add(tf.keras.layers.RandomRotation(40))
+  preprocessing_model.add(tf.keras.layers.RandomTranslation(0, 0.2))
+  preprocessing_model.add(tf.keras.layers.RandomTranslation(0.2, 0))
+  preprocessing_model.add(tf.keras.layers.RandomZoom(0.2, 0.2))
+  preprocessing_model.add(tf.keras.layers.RandomFlip(mode="horizontal"))
+train_ds = train_ds.map(lambda images, labels: (preprocessing_model(images), labels))
+
+val_ds = build_dataset("validation")
+valid_size = val_ds.cardinality().numpy()
+val_ds = val_ds.unbatch().batch(BATCH_SIZE)
+val_ds = val_ds.map(lambda images, labels: (normalization_layer(images), labels))
+
+do_fine_tuning = False
+
+print("Building model with", model_handle)
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=IMAGE_SIZE + (3,)),
+    hub.KerasLayer(model_handle, trainable=do_fine_tuning),
+    tf.keras.layers.Dropout(rate=0.2),
+    tf.keras.layers.Dense(len(class_names),
+                          kernel_regularizer=tf.keras.regularizers.l2(0.0001))
+])
+model.build((None,) + IMAGE_SIZE + (3,))
+model.summary()
+
+model.compile(
+  optimizer=tf.keras.optimizers.SGD(learning_rate=0.005, momentum=0.9),
+  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True, label_smoothing=0.1),
+  metrics=['accuracy'])
+
+steps_per_epoch = train_size // BATCH_SIZE
+validation_steps = valid_size // BATCH_SIZE
+hist = model.fit(
+    train_ds,
+    epochs=5, steps_per_epoch=steps_per_epoch,
+    validation_data=val_ds,
+    validation_steps=validation_steps).history
+
+plt.figure()
+plt.ylabel("Loss (training and validation)")
+plt.xlabel("Training Steps")
+plt.ylim([0,2])
+plt.plot(hist["loss"])
+plt.plot(hist["val_loss"])
+
+plt.figure()
+plt.ylabel("Accuracy (training and validation)")
+plt.xlabel("Training Steps")
+plt.ylim([0,1])
+plt.plot(hist["accuracy"])
+plt.plot(hist["val_accuracy"])
+
+# Visualize the image along with true and predicted labels
+x, y = next(iter(val_ds))
+image = x[0, :, :, :]
+true_index = np.argmax(y[0])
+
+# Expand the validation image to (1, 224, 224, 3) before predicting the label
+prediction_scores = model.predict(np.expand_dims(image, axis=0))
+predicted_index = np.argmax(prediction_scores)
+
+plt.figure()
+plt.imshow(image)
+plt.title(f'True label: {class_names[true_index]}, Predicted label: {class_names[predicted_index]}')
+plt.axis('off')
+plt.show()
+
+# Print true and predicted labels
+print("True label:", class_names[true_index])
+print("Predicted label:", class_names[predicted_index])
+
+saved_model_path = f"/tmp/saved_flowers_model_{model_name}"
+tf.saved_model.save(model, saved_model_path)
